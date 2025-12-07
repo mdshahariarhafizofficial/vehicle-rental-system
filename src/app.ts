e4ed32f1ff735ae872c252597e116e004da550b6
mdshahariarhafizofficial <shahariar.works@gmail.com>
2025-12-07 19:11:34 +0600

    try to fix deployment issue

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import bookingRoutes from './routes/booking.routes';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
